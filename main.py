c6dd539c54ec919e991eada12f22abecb25a16ce
iancdesponds <iancdesponds@gmail.com>
2024-08-13 17:06:09 -0300

    feat: projeto completo

---
+++ main.py
@@ -0,0 +1,50 @@
+from flask import Flask, jsonify, request
+import numpy as np
+from enigma_gubs_ian.myfunctions import *
+
+app = Flask("Enigmas API - Ian e Gubs")
+
+@app.route("/para_one_hot", methods=["POST"])
+def api_para_one_hot():
+    data = request.json
+    msg = data["msg"]
+    one_hot = para_one_hot(msg)
+    return jsonify({"one_hot": one_hot.tolist()}, 200)
+
+@app.route("/para_string", methods=["POST"])
+def api_para_string():
+    data = request.json
+    M = np.array(data["M"])
+    string = para_string(M=M)
+    return jsonify({"string": string}, 200)
+
+@app.route("/cifrar", methods=["POST"])
+def api_cifrar():
+    data = request.json
+    msg = data["msg"]
+    encrypted = cifrar(msg, P)
+    return jsonify({"encrypted": encrypted}, 200)
+
+@app.route("/de_cifrar", methods=["POST"])
+def api_de_cifrar():
+    data = request.json
+    msg = data["msg"]
+    decrypted = de_cifrar(msg, P)   
+    return jsonify({"decrypted": decrypted}, 200)
+
+@app.route("/enigma", methods=["POST"])
+def api_enigma():
+    data = request.json
+    msg = data["msg"]
+    encrypted = enigma(msg, P, E)
+    return jsonify({"encrypted": encrypted}, 200)
+
+@app.route("/de_enigma", methods=["POST"])
+def api_de_enigma():
+    data = request.json
+    msg = data["msg"]
+    decrypted = de_enigma(msg, P, E)
+    return jsonify({"decrypted": decrypted}, 200)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ tests.py
@@ -0,0 +1,32 @@
+from enigma_gubs_ian.myfunctions import *
+import numpy as np
+
+permutation = np.random.permutation(26)
+id_matrix = np.eye(26)
+P = id_matrix[permutation]
+
+permutation2 = np.random.permutation(26)
+id_matrix2 = np.eye(26)
+E = id_matrix2[permutation2]
+
+# Teste das funções
+msg_original = input("Digite a mensagem que deseja testar: ")
+print("Mensagem original:", msg_original)
+print("")
+
+one_hot = para_one_hot(msg_original)
+print("Para one-hot:\n", one_hot)
+string = para_string(one_hot)
+print("One-hot para string:", string)
+print("")
+
+msg_cifrada = cifrar(msg_original, P)
+print("Mensagem cifrada:", msg_cifrada)
+msg_decifrada = de_cifrar(msg_cifrada, P)
+print("Mensagem decifrada:", msg_decifrada)
+print("")
+
+msg_enigmada = enigma(msg_original, P, E)
+print("Mensagem enigmada:", msg_enigmada)
+msg_de_enigmada = de_enigma(msg_enigmada, P, E)
+print("Mensagem de-enigmada:", msg_de_enigmada)
